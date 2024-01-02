b091bea975704f0051403d10577a29d087fecd49
raymond <raymond.1zou@gmail.com>
2024-01-01 16:36:18 -0800

    Initial commit

---
+++ init.lua
@@ -0,0 +1,72 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.keymap.set("i", "kj", "<Esc>")
+
+-- show lines
+vim.cmd.set("number")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+
+	{
+		"folke/tokyonight.nvim",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("tokyonight-night")
+		end,
+	},
+
+	-- lsp-zero.nvim
+	{ "williamboman/mason.nvim" },
+	{ "williamboman/mason-lspconfig.nvim" },
+	{ "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
+	{ "neovim/nvim-lspconfig" },
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/nvim-cmp" },
+	{ "L3MON4D3/LuaSnip" },
+
+	-- conform (for formatting)
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+	},
+
+	-- telescope
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		-- or                              , branch = '0.1.x',
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+
+	-- treesitter
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+})
+
+require("mason").setup()
+require("mason-lspconfig").setup()
+
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+	},
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
+})
