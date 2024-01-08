def08b7160ce6f258c8d266b7ac402f80145fd8f
Raymond Zou <raymond.1zou@gmail.com>
2024-01-07 22:51:19 -0800

    change plugins to modules

--- init.lua
+++ init.lua
@@ -1,12 +1,7 @@
-print("hello")
+require("options")
 
 vim.g.maplocalleader = " "
 vim.keymap.set("i", "kj", "<Esc>")
-
--- show lines
-vim.cmd.set("number")
-vim.cmd.set("relativenumber")
-vim.api.nvim_set_option("clipboard", "unnamed") -- global clipboard
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -32,80 +27,32 @@ require("lazy").setup({
 	},
 
 	-- commenting
-	{ "numToStr/Comment.nvim" },
+	{
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+		end,
+	},
 	{ "nvim-lualine/lualine.nvim" },
 
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"hrsh7th/cmp-buffer", -- buffer completions
-			"hrsh7th/cmp-path", -- buffer completions
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
-		},
-	},
 	{ "hrsh7th/cmp-nvim-lsp" },
-	{ "neovim/nvim-lspconfig" },
-	-- { "L3MON4D3/LuaSnip" },
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("lspconfig").lua_ls.setup({})
+		end,
+	},
 
-	{ "williamboman/mason.nvim" },
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
 	{ "williamboman/mason-lspconfig.nvim" },
-
-	-- conform (for formatting)
-	{
-		"stevearc/conform.nvim",
-		opts = {},
-	},
-
-	-- telescope
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
-		-- or                              , branch = '0.1.x',
-		dependencies = { "nvim-lua/plenary.nvim" },
-	},
 
 	-- treesitter
 	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-})
 
-require("mason").setup()
-require("conform").setup({
-	formatters_by_ft = {
-		lua = { "stylua" },
-	},
+	{ import = "plugins" },
 })
-require("Comment").setup()
-
-local cmp = require("cmp")
-cmp.setup({
-	snippet = {
-		expand = function(args)
-			require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-		end,
-	},
-	mapping = cmp.mapping.preset.insert({
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
-		["<Tab>"] = function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			else
-				fallback()
-			end
-		end,
-	}),
-	sources = {
-		{ name = "nvim_lsp" },
-		{ name = "buffer" },
-		{ name = "path" },
-	},
-})
-
-vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*",
-	callback = function(args)
-		require("conform").format({ bufnr = args.buf })
-	end,
-})
-
-require("lspconfig").lua_ls.setup({})
---
+++ lua/options.lua
@@ -0,0 +1,14 @@
+vim.cmd.set("relativenumber")
+
+vim.o.hlsearch = false
+
+vim.wo.number = true
+
+vim.o.mouse = "a"
+
+vim.o.clipboard = "unnamedplus"
+
+vim.o.undofile = true
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
