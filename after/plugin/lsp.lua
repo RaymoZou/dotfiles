5e5f45b479d6c3d307e7f5b5c9caaa48089b9605
Raymond Zou <raymond.1zou@gmail.com>
2024-01-11 10:07:58 -0800

    add keymap for lsp hover

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,30 +4,30 @@ require("neodev").setup()
 
 local servers = { -- lspconfig server names (add servers here as needed)
 	lua_ls = {
-		Lua = { workspace = { checkThirdParty = false }, telemetry = { enable = false } },
+		-- Lua = { workspace = { checkThirdParty = false }, telemetry = { enable = false } },
 	},
 	clangd = {},
 	tsserver = {},
 }
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
--- function taken form kickstart.nvim [https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua]
+-- function taken from kickstart.nvim [https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua]
 local on_attach = function(_, bufnr)
 	local ts_builtin = require("telescope.builtin")
 	vim.keymap.set("n", "gd", ts_builtin.lsp_definitions, { buffer = bufnr, desc = "Go to Definition" })
 	vim.keymap.set("n", "gd", ts_builtin.lsp_definitions, { buffer = bufnr, desc = "Go to Definition" })
 	vim.keymap.set("n", "gr", ts_builtin.lsp_references, { buffer = bufnr, desc = "Go to References" })
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr, desc = "Hover" })
 	vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "Format with LSP" })
 end
 
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local handler = {
 	function(server_name)
 		require("lspconfig")[server_name].setup({
 			on_attach = on_attach,
 			settings = servers[server_name],
 			filetypes = (servers[server_name] or {}).filetypes,
-			capabilities = capabilties,
+			capabilities = capabilities,
 		})
 	end,
 }
