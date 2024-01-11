c88276bc3c323864f0e7d2052cc3a5e35299d9ff
Raymond Zou <raymond.1zou@gmail.com>
2024-01-11 14:28:37 -0800

    change settings

--- lua/options.lua
+++ lua/options.lua
@@ -1,14 +1,12 @@
-vim.cmd.set("relativenumber")
-
+vim.o.relativenumber = true
+vim.o.number = true
 vim.o.hlsearch = false
-
-vim.wo.number = true
-
 vim.o.mouse = "a"
-
 vim.o.clipboard = "unnamedplus"
-
 vim.o.undofile = true
-
 vim.o.ignorecase = true
 vim.o.smartcase = true
+vim.o.splitright = true
+vim.o.splitbelow = true
+vim.o.autochdir = true
+vim.g.netrw_keepdir = 0
