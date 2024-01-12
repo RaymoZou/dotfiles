4a4d49ac7c01c9b7add273daf552e38d3e5045d2
Raymond Zou <raymond.1zou@gmail.com>
2024-01-12 00:46:06 -0800

    add scroll offset and other options

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,9 @@
 vim.o.relativenumber = true
 vim.o.number = true
+
 vim.o.hlsearch = false
+vim.o.incsearch = true -- incremental search
+
 vim.o.mouse = "a"
 vim.o.clipboard = "unnamedplus"
 vim.o.undofile = true
@@ -8,5 +11,12 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.splitright = true
 vim.o.splitbelow = true
-vim.o.autochdir = true
+vim.o.autochdir = true -- not sure if I want to keep these settings yet
 vim.g.netrw_keepdir = 0
+
+vim.o.smartindent = true
+
+vim.o.termguicolors = true
+vim.o.scrolloff = 8
+vim.o.updatetime = 50
+vim.o.wrap = false;
