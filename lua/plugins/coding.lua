224c3e2a4ca6d3731b65d6eff05c4fc7a924e594
Raymond Zou <raymond.1zou@gmail.com>
2024-01-12 22:24:40 -0800

    add plugin for cursor dependent commenting

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -32,10 +32,15 @@ return {
 		end,
 	},
 	-- commenting
+	{ "JoosepAlviste/nvim-ts-context-commentstring" },
 	{
 		"numToStr/Comment.nvim",
 		config = function()
-			require("Comment").setup()
+			require("Comment").setup({
+				pre_hook = function()
+					return vim.bo.commentstring
+				end,
+			})
 		end,
 	},
 }
