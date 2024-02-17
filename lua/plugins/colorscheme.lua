4b962bb5575cd840224685824d7c6eed5ae43229
raymond <raymond.1zou@gmail.com>
2024-02-17 06:38:06 -0800

    add colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,16 @@
+-- various color schemes with onedark loaded by default
+
 return {
-	"navarasu/onedark.nvim",
-	priority = 1000,
-	config = function()
-		require("onedark").setup({
-			style = "darker",
-		})
-		require("onedark").load()
-	end,
+    {
+        "navarasu/onedark.nvim",
+        priority = 1000,
+        config = function()
+            require("onedark").setup({
+                style = "darker",
+            })
+            require("onedark").load()
+        end,
+    },
+    { "folke/tokyonight.nvim" }, -- tokyonight-{night, storm, day, moon}
+    { "catppuccin/nvim" }   -- catppucin-{latte, frappe, macchiato, mocha}
 }
