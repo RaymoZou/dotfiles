07c95286de4a560f42232ddfb779d39b7ff4e4bb
Raymond Zou <raymond.1zou@gmail.com>
2024-02-24 07:48:32 -0800

    move lualine set up to after directory

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,12 +1,8 @@
 -- plugins relating to UI
--- TODO: add plugin for visualizing git differences in files
 return {
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("lualine").setup()
-		end,
-	},
-	{ "j-hui/fidget.nvim", opts = {} },
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
+    { "j-hui/fidget.nvim", opts = {} },
 }
