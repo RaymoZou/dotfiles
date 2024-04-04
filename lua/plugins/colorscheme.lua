a9ed81fc5182754af95abd68b2f47567f5fa5cb5
RaymoZou <raymond.1zou@gmail.com>
2024-04-04 08:31:30 -0700

    catppuccin color scheme by default

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,14 +3,20 @@
 return {
     {
         "navarasu/onedark.nvim",
-        priority = 1000,
-        config = function()
-            require("onedark").setup({
-                style = "darker",
-            })
-            require("onedark").load()
-        end,
+        -- priority = 1000,
+        -- config = function()
+        --     require("onedark").setup({
+        --         style = "darker",
+        --     })
+        --     require("onedark").load()
+        -- end,
     },
     { "folke/tokyonight.nvim" }, -- tokyonight-{night, storm, day, moon}
-    { "catppuccin/nvim" }   -- catppucin-{latte, frappe, macchiato, mocha}
+    {
+        "catppuccin/nvim",
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme "catppuccin"
+        end,
+    } -- catppucin-{latte, frappe, macchiato, mocha}
 }
