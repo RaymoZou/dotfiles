6ce631b4881f7c1a2f5369bae12fb89ba3ec40f5
Raymond <raymond.1zou@gmail.com>
2024-05-09 22:09:08 -0700

    block cursor for common modes

--- lua/options.lua
+++ lua/options.lua
@@ -35,6 +35,7 @@ vim.o.scrolloff = 8
 vim.o.updatetime = 50
 vim.o.wrap = false
 vim.o.breakindent = true
+vim.o.guicursor = "v-i-n-c:block" -- TODO: change color based on mode
 
 -- remaps (thanks ThePrimeAgen)
 vim.keymap.set("i", "kj", "<Esc>")
