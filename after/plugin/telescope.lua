244130629c8df05307c51f715533e0a89b00fc2a
RaymoZou <raymond.1zou@gmail.com>
2025-02-15 15:53:13 -0800

    add telescope keybind for changing colorschemes

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,26 +1,26 @@
 local builtin = require("telescope.builtin")
 
--- thank you TJ
+-- return the git root, or the cwd if not found
 local function find_git_root()
-  -- Use the current buffer's path as the starting point for the git search
-  local current_file = vim.api.nvim_buf_get_name(0)
-  local current_dir
-  local cwd = vim.fn.getcwd()
-  -- If the buffer is not associated with a file, return nil
-  if current_file == '' then
-    current_dir = cwd
-  else
-    -- Extract the directory from the current file's path
-    current_dir = vim.fn.fnamemodify(current_file, ':h')
-  end
+    -- Use the current buffer's path as the starting point for the git search
+    local current_file = vim.api.nvim_buf_get_name(0)
+    local current_dir
+    local cwd = vim.fn.getcwd()
+    -- If the buffer is not associated with a file, return nil
+    if current_file == '' then
+        current_dir = cwd
+    else
+        -- Extract the directory from the current file's path
+        current_dir = vim.fn.fnamemodify(current_file, ':h')
+    end
 
-  -- Find the Git root directory from the current file's path
-  local git_root = vim.fn.systemlist('git -C ' .. vim.fn.escape(current_dir, ' ') .. ' rev-parse --show-toplevel')[1]
-  if vim.v.shell_error ~= 0 then
-    print 'Not a git repository. Searching on current working directory'
-    return cwd
-  end
-  return git_root
+    -- Find the Git root directory from the current file's path
+    local git_root = vim.fn.systemlist('git -C ' .. vim.fn.escape(current_dir, ' ') .. ' rev-parse --show-toplevel')[1]
+    if vim.v.shell_error ~= 0 then
+        print 'Not a git repository. Searching on current working directory'
+        return cwd
+    end
+    return git_root
 end
 
 local function live_grep_git_root()
@@ -32,16 +32,17 @@ local function live_grep_git_root()
     end
 end
 
--- TODO: create live_file_git_root() to find files in git
--- useful for when autochdir is set to true
-
 
 vim.api.nvim_create_user_command('LiveGrepGitRoot', live_grep_git_root, {})
 
+
+vim.keymap.set("n", "<leader>sf", builtin.git_files, { desc = "[S]earch [f]iles on Git Root" })        -- based on current git root
 vim.keymap.set("n", "<leader>sF", builtin.find_files, { desc = "[S]earch [F]iles" })
-vim.keymap.set("n", "<leader>sf", builtin.git_files, { desc = "[S]earch [f]iles on Git Root" }) -- based on current git root
-vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch [G]rep" }) -- based on current directory 
+vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch [G]rep" })                     -- based on current directory
+vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' }) -- based on current git root or cwd
 vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "[S]earch [B]uffers" })
 vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp files" })
+vim.keymap.set("n", "<leader>cs", function()
+    builtin.colorscheme({ enable_preview = true })
+end, { desc = "[C]olor [S]chemes" })
 vim.keymap.set("n", "<leader>?", builtin.oldfiles, { desc = "Recently Opened Files" })
-vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' }) -- based on current git root
