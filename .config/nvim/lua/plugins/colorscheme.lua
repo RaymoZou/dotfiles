96164839289821c935d1162fe7ecca9c49e071d3
Raymond <raymond.1zou@gmail.com>
2025-07-03 14:06:37 -0700

    style: default colorscheme moonfly -> catppuccin

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -22,15 +22,15 @@ return {
             opts = {
                 transparent_background = true
             },
+            init = function()
+                vim.cmd.colorscheme("catppuccin")
+            end,
         },
         {
             "bluz71/vim-moonfly-colors",
             name = "moonfly",
             lazy = false,
             priority = 1000,
-            init = function()
-                vim.cmd.colorscheme("moonfly")
-            end,
         },
     },
 }
